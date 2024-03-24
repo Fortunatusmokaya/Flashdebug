84490b005c2b4e9d79879c9e34a3f6fcbadda415
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-24 18:45:44 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -35,7 +35,7 @@ zokou({ nomCom: 'uptime',
 
   }
 );
-/*
+
 
 zokou({ nomCom: 'ss',
     desc: 'screenshots website',
@@ -47,7 +47,24 @@ zokou({ nomCom: 'ss',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-if (! */
+    if (!arg || arg.length === 0) return repondre("provide a link...");
+
+         const linkk = arg.join(' ');
+
+try {
+
+let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURIComponent(linkk)}&dimension=720x720`;
+
+let res = await getBuffer(linkkk);
+
+await zk.sendMessage(m.chat, { image: res }, { quoted: ms });
+
+} catch (error) {
+    return repondre("something went wrong while fetching screenshot");
+
+}
+}
+);
 
 
 zokou({ nomCom: 'ping',
