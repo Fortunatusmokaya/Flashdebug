4700e022b570aa3f82500246f019f6a64d2a5c34
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-24 19:07:14 +0300

    Update Function.js

--- framework/dl/Function.js
+++ framework/dl/Function.js
@@ -76,6 +76,27 @@ exports.webp2mp4File=async(path) =>{
 	})
 }
 
+
+exports.getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (err) {
+		return err
+	}
+}
+
+
 exports.fetchUrl = async (url, options) => {
     try {
         options ? options : {}
