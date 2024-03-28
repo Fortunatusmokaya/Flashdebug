fda80af0a08b77dcd0c7b7fb8a6c676cbb2d3066
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-28 16:23:20 +0300

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -3,6 +3,37 @@ const axios =require("axios");
 const Genius = require("genius-lyrics"); 
  const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
+
+
+zokou({ nomCom: "poll",
+        reaction: "✨",
+        categorie: "General" }, async (dest, zk, commandeOptions) => {
+    
+    const { repondre, arg, ms } = commandeOptions; 
+const polll = arg.join(' ');
+
+const polll = arg.join(' ');
+
+let [poll, opt] = polll.split("|")
+
+if (polll.split("|") < 2)
+                return repondre(`Incorrect format.\nExample: poll what is 1+1|2, 3, 4`);
+
+let options = []
+            for (let i of opt.split(',')) {
+                options.push(i)
+            }
+            await zk.sendMessage(dest, {
+                poll: {
+                    name: poll,
+                    values: options
+                }
+            })
+
+})
+
+ 
+        
 zokou({ nomCom: "lyrics",
         reaction: "✨",
         categorie: "Search" }, async (dest, zk, commandeOptions) => {
