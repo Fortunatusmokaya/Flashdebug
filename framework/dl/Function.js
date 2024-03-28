6452cfeb767cac2f4f4e22d722adf1ff6b67fd67
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-28 15:38:15 +0300

    Update Function.js

--- framework/dl/Function.js
+++ framework/dl/Function.js
@@ -77,6 +77,27 @@ exports.webp2mp4File=async(path) =>{
 }
 
 
+
+
+exports.mediafireDl = async (url, options) => {
+    try {
+        options = options ? options : {};
+        const res = await axios.get(url, options);
+        const $ = cheerio.load(res.data);
+        const hasil = [];
+        const link = $('a#downloadButton').attr('href');
+        const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '');
+        const seplit = link.split('/');
+        const nama = seplit[5];
+        mime = nama.split('.');
+        mime = mime[1];
+        hasil.push({ nama, mime, size, link });
+        return hasil;
+    } catch (err) {
+        return err;
+    }
+};
+
 exports.getBuffer = async (url, options) => {
 	try {
 		options ? options : {}
